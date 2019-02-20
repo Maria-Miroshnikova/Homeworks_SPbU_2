b2b0af9c3daddee4f3027ec45fe99cbcb34b376f
mari.mirosh <mari.mirosh@bk.ru>
2019-02-20 18:27:38 +0300

    done

---
+++ Homework_1/1_1_ex/1_1_ex/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, input the number to count the factorial:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Wrong data!");
+            }
+            else
+            {
+                Console.WriteLine($"The answer is: {Factorial(number)}.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
