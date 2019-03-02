e6bb50c47d5f1e1e598040811cc88b36777b6709
mari.mirosh <mari.mirosh@bk.ru>
2019-03-02 11:28:22 +0300

    after discussion

--- Homework_2/2_1_ex/2_1_ex/Interface.cs
+++ Homework_2/2_1_ex/2_1_ex/Interface.cs
@@ -1,185 +1,11 @@
 ﻿using System;
 using System.IO;
 
-namespace List
+namespace ListNamespace
 {
-    struct Answer
-    {
-        public int answer;
-        public bool success;
-    }
-
-    class ListElement
-    {
-        public ListElement(int data, ListElement next)
-        {
-            this.data = data;
-            this.next = next;
-        }
-
-        private int data;
-        private ListElement next;
-
-        public ListElement GetNext()
-        {
-            return next;
-        }
-
-        public int GetData()
-        {
-            return data;
-        }
-
-        public void ChangeNext(ListElement newNext)
-            => this.next = newNext;
-
-        public void ChangeData(int newData)
-            => this.data = newData;
-
-    }
-
-    class List
-    {
-        public List()
-        {
-            this.head = null;
-            this.size = 0;
-        }
-
-        private ListElement head;
-        private int size;
-
-        public bool IsEmpty()
-        {
-            return (head == null);
-        }
-
-        public int Size()
-        {
-            return size;
-        }
-
-        private ListElement FindPrevious(int index)
-        {
-            int i = 1;
-            ListElement element = head;
-            while (i < index - 1)
-            {
-                element = element.GetNext();
-                ++i;
-            }
-            return element;
-        }
-
-        public bool Add(int index, int data)
-        {
-            if ((index > size + 1) || (index < 1))
-            {
-                return false;
-            }
-
-            var newElement = new ListElement(data, null);
-            ++this.size;
-
-            if (index == 1)
-            {
-                newElement.ChangeNext(head);
-                this.head = newElement;
-                return true;
-            }
-
-            ListElement element = FindPrevious(index);
-
-            newElement.ChangeNext(element.GetNext());
-            element.ChangeNext(newElement);
-            return true;
-        }
-
-        public Answer Get(int index)
-        {
-            var result = new Answer();
-
-            if ((index > size) || (index < 1))
-            {
-                result.success = false;
-                return result;
-            }
-            
-            if (index == 1)
-            {
-                result.answer = head.GetData();
-                result.success = true;
-                return result;
-            }
-
-            result.answer = FindPrevious(index).GetNext().GetData();
-            result.success = true;
-            return result;
-        }
-
-        public bool Change(int index, int data)
-        {
-            if ((index > size) || (index < 1))
-            {
-                return false;
-            }
-
-            if (index == 1)
-            {
-                head.ChangeData(data);
-                return true;
-            }
-
-            FindPrevious(index).GetNext().ChangeData(data);
-            return true;
-        }
-
-        public int[] GetAll()
-        {
-            var outputList = new int[size];
-            for (int i = 1; i <= size; ++i)
-            {
-                outputList[i - 1] = this.Get(i).answer;
-            }
-            return outputList;
-        }
-
-        public bool Delete(int index)
-        {
-            if ((index > size) || (index < 1))
-            {
-                return false;
-            }
-
-            --this.size;
-
-            if (index == 1)
-            {
-                this.head = head.GetNext();
-                return true;
-            }
-
-            ListElement element = FindPrevious(index);
-            element.ChangeNext(element.GetNext().GetNext());
-            return true;
-        }
-
-        public bool DeleteAll()
-        {
-            if (IsEmpty())
-            {
-                return false;
-            }
-
-            this.head = null;
-            this.size = 0;
-            return true;
-        }
-    }
-
     class Interface
     {
-        static void outputArray(int[] array)
+        static void OutputArray(int[] array)
         {
             foreach (var element in array)
             {
@@ -187,7 +13,7 @@ namespace List
             }
         }
 
-        static void help()
+        static void Help()
         {
             Console.Write("\n");
             using (var helpFile = new StreamReader("HELP.txt"))
@@ -200,7 +26,7 @@ namespace List
         {
             string[] commands = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "HELP" };
 
-            help();
+            Help();
             Console.Write("\nPlease, enter the command: ");
             string command = Console.ReadLine();
 
@@ -218,7 +44,9 @@ namespace List
                     Console.Write("Please, enter the number to add: ");
                     parameter1 = Console.ReadLine();
                     if (!Int32.TryParse(parameter1, out parameterInt1))
+                    {
                         Console.WriteLine("\nError: incorrect data!");
+                    }
 
                     else
                     {
@@ -237,19 +65,23 @@ namespace List
 
                     parameter1 = Console.ReadLine();
                     if (!Int32.TryParse(parameter1, out parameterInt1))
+                    {
                         Console.WriteLine("\nError: incorrect data!");
+                    }
                     else if (!list.Delete(parameterInt1))
+                    {
                         Console.WriteLine("\nError: index out of list!");
+                    }
                 }
 
                 else if (command == commands[3])
                 {
-                    Console.Write("The size of array is: {0}\n", list.Size());
+                    Console.Write("The size of array is: {0}\n", list.Size);
                 }
 
                 else if (command == commands[4])
                 {
-                    if (list.IsEmpty())
+                    if (list.IsEmpty)
                         Console.WriteLine("The list is empty!");
                     else
                         Console.WriteLine("The list is NOT empty!");
@@ -265,7 +97,7 @@ namespace List
 
                     else
                     {
-                        Answer result = list.Get(parameterInt1);
+                        var result = list.Get(parameterInt1);
 
                         if (!result.success)
                             Console.WriteLine("\nError: index out of list!");
@@ -295,12 +127,12 @@ namespace List
 
                 else if (command == commands[7])
                 {
-                    if (list.IsEmpty())
+                    if (list.IsEmpty)
                         Console.WriteLine("The list is empty!");
                     else
                     {
                         Console.WriteLine("The list is:\n");
-                        outputArray(list.GetAll());
+                        OutputArray(list.GetAll());
                     }
                 }
 
@@ -312,7 +144,7 @@ namespace List
 
                 else if (command == commands[9])
                 {
-                    help();
+                    Help();
                 }
 
                 else
