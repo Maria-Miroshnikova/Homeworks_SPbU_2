14a4d66a62cbba04f59dd4d909650c5713c66d07
mari.mirosh <mari.mirosh@bk.ru>
2019-04-26 15:07:13 +0300

    best running away button

---
+++ test_1_2/test_1_2/FollowButton.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace test_1_2
+{
+    partial class FollowButton
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFollow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFollow
+            // 
+            this.buttonFollow.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonFollow.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFollow.ForeColor = System.Drawing.Color.Black;
+            this.buttonFollow.Location = new System.Drawing.Point(12, 12);
+            this.buttonFollow.Name = "buttonFollow";
+            this.buttonFollow.Size = new System.Drawing.Size(120, 56);
+            this.buttonFollow.TabIndex = 0;
+            this.buttonFollow.Text = "follow me!";
+            this.buttonFollow.UseVisualStyleBackColor = false;
+            this.buttonFollow.Click += new System.EventHandler(this.buttonFollow_Click);
+            this.buttonFollow.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonFollow_MouseMove);
+            // 
+            // FollowButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonFollow);
+            this.MaximumSize = new System.Drawing.Size(816, 488);
+            this.MinimumSize = new System.Drawing.Size(816, 488);
+            this.Name = "FollowButton";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFollow;
+    }
+}
+
---
+++ test_1_2/test_1_2/FollowButton.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_1_2
+{
+    /// <summary>
+    /// This class presents the window with the button, which runs away from mouse when in comes up.
+    /// The window closes, when the button is clicked.
+    /// </summary>
+    public partial class FollowButton : Form
+    {
+        public FollowButton()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This function makes application close when user clicks the button.
+        /// </summary>
+        private void buttonFollow_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// This function makes the button run away from the mouse when it comes up to the button.
+        /// </summary>
+        private void buttonFollow_MouseMove(object sender, MouseEventArgs e)
+        {
+            int change = 2;
+
+            int boardLeftUpX = 12;
+            int boardLeftUpY = 12;
+
+            int boardRightDownX = 668;
+            int boardRightDownY = 382;
+
+            if (buttonFollow.Location.X == boardLeftUpX)
+            {
+                if (buttonFollow.Location.Y + change <= boardRightDownY)
+                {
+                    buttonFollow.Location = new Point(buttonFollow.Location.X, buttonFollow.Location.Y + change);
+                }
+                else
+                {
+                    buttonFollow.Location = new Point(boardLeftUpX, boardRightDownY);
+                }
+            }
+
+            if (buttonFollow.Location.Y == boardRightDownY)
+            {
+                if (buttonFollow.Location.X + change <= boardRightDownX)
+                {
+                    buttonFollow.Location = new Point(buttonFollow.Location.X + change, buttonFollow.Location.Y);
+                }
+                else
+                {
+                    buttonFollow.Location = new Point(boardRightDownX, boardRightDownY);
+                }
+            }
+
+            if (buttonFollow.Location.X == boardRightDownX)
+            {
+                if (buttonFollow.Location.Y - change >= boardLeftUpY)
+                {
+                    buttonFollow.Location = new Point(buttonFollow.Location.X, buttonFollow.Location.Y - change);
+                }
+                else
+                {
+                    buttonFollow.Location = new Point(boardRightDownX, boardLeftUpY);
+                }
+            }
+
+            if (buttonFollow.Location.Y == boardLeftUpY)
+            {
+                if (buttonFollow.Location.X - change >= boardLeftUpX)
+                {
+                    buttonFollow.Location = new Point(buttonFollow.Location.X - change, buttonFollow.Location.Y);
+                }
+                else
+                {
+                    buttonFollow.Location = new Point(boardLeftUpX, boardLeftUpY);
+                }
+            }
+        }
+
+        
+    }
+}
