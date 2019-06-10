b8305659b20b3192277123fa33dcce56850417c5
Maria Miroshnikova <mari.mirosh@bk.ru>
2019-06-10 18:17:04 +0300

    add comments to exception

--- Homework_2/2_3_ex/2_3_ex/TooMuchElementsInStackException.cs
+++ Homework_2/2_3_ex/2_3_ex/TooMuchElementsInStackException.cs
@@ -2,6 +2,9 @@
 
 namespace StackNameSpace
 {
+    /// <summary>
+    /// This exception should be trown when someone adds to stack more then 1000 elements.
+    /// </summary>
     public class TooMuchElementsInStackException : Exception
     {
         public TooMuchElementsInStackException()
